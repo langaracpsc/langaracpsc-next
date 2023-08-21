b7766ec273c0d3f00be4b3433f43cd50b85ab2ee
rishit-singh <rsrishitsingh@gmail.com>
2023-08-21 01:12:28 -0700

    Fixed Header scaling

--- frontend/src/app/hacks/Header.tsx
+++ frontend/src/app/hacks/Header.tsx
@@ -3,18 +3,19 @@ import {IconLabel} from "@/app/hacks/IconLabel";
 
 export function Header()
 {
-    return <div className = {"bg-hacks-nav-black"}>
+    return <div className = {"bg-hacks-nav-black w-full"}>
         <div className = {"grid grid-rows-2 h-full"}>
             <div className={"grid items-end"}>
                 <div className={"flex flex-col"}>
-                    <div className={"text-5xl self-center"}> Langara <span className={"text-lang-orange"}>Hacks</span></div>
+                    <div className={"text-5xl max-[850px]:text-4xl self-center"}> Langara <span className={"text-lang-orange"}>Hacks</span></div>
                 </div>
             </div>
+            
             <div className={"grid grid-cols-2"}>
-                <div className={" self-center"}>
+                <div className={"justify-self-end self-center w-[60vh] max-[850px]:w-[30vh]"}>
                     <IconLabel iconSrc={"/location_icon.png"} label={"T Gallery, Langara"} height={25} width={25}/>
-                </div>
-                <div className={"justify-self-start self-center"}>
+                </div> 
+                <div className={"justify-self-start self-center w-[40vh] max-[850px]:w-[20vh]"}>
                     <IconLabel iconSrc={"/calender_icon.png"} label={"Sept 23rd, 2023"} height={25} width={25}/>
                 </div>
             </div>
