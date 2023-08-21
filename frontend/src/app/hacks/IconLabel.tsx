43a7e69c913e6e1b6d978332c2b3d4194e7cb2a0
rishit-singh <rsrishitsingh@gmail.com>
2023-08-21 00:00:05 -0700

    Fixed CI build errors

--- frontend/src/app/hacks/IconLabel.tsx
+++ frontend/src/app/hacks/IconLabel.tsx
@@ -1,10 +1,9 @@
 import Image from "next/image";
 
-export function IconLabel({iconSrc, label, height, width, fontSize})
+export function IconLabel({iconSrc, label, height, width}: {iconSrc: string, label: string, height: number, width: number})
 {
-    
     return <div className={"grid grid-cols-2"}>
-        <Image src={iconSrc} alt={"Image"} height={height as number} width={width as number} className={"self-center justify-self-end"}></Image>
+        <Image src={iconSrc as string} alt={"Image"} height={height as number} width={width as number} className={"self-center justify-self-end"}></Image>
         <span className={"text-lg text-white self-center ml-2"}>{label}</span>
     </div>; 
 }
