e294545340748f1c998db74c142afb0d890783c0
rishit-singh <rsrishitsingh@gmail.com>
2023-08-22 14:53:25 -0700

    Added more spacing

--- frontend/src/app/hacks/Body.tsx
+++ frontend/src/app/hacks/Body.tsx
@@ -4,11 +4,11 @@ import {SocialIcons} from "@/app/SocialIcons";
 export function Body()
 {
     return <div className={"grid-cols-2 bg-hacks-bg-black"}> 
-            <div className={"flex flex-col items-center gap-[5vh] max-[850px]:gap-[8vh] mt-10 max-[850px]:mt-[8vh]"}>
+            <div className={"flex flex-col items-center gap-[7vh] max-[850px]:gap-[8vh] mt-[8vh] max-[850px]:mt-[9vh]"}>
                 <span className={"text-3xl max-[850px]:text-xl"}>Registrations open soon!</span>
                 <Timer EndTime={new Date(2023, 8, 23, 8, 0, 0)}/> 
             </div>
-            <div className={"flex flex-row justify-center h-max gap-5 mt-[10vh] max-[850px]:mt-[25vh]"}>
+            <div className={"flex flex-row justify-center h-max gap-5 mt-[15vh] max-[850px]:mt-[25vh]"}>
                 <SocialIcons/>
             </div>
         </div>
--- frontend/src/app/hacks/IconLabel.tsx
+++ frontend/src/app/hacks/IconLabel.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export function IconLabel({iconSrc, label, height, width}: {iconSrc: string, label: string, height: number, width: number})
 {
     return <div className={"grid grid-cols-2"}>
-        <Image src={iconSrc as string} alt={"Image"} height={height as number} width={width as number} className={"self-center justify-self-end"}></Image>
+        <Image src={iconSrc} alt={"Image"} height={height} width={width} className={"self-center justify-self-end"}></Image>
         <span className={"text-lg max-[850px]:text-xs text-white self-center ml-2"}>{label}</span>
     </div>; 
 }
