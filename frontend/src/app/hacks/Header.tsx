03ec78e353058704d0b3c82d0dcc5ebbacc84a16
Anderson T <tseng.andersonn@gmail.com>
2023-08-22 20:03:33 -0700

    add tagline

--- frontend/src/app/hacks/Header.tsx
+++ frontend/src/app/hacks/Header.tsx
@@ -8,12 +8,14 @@ export function Header()
             <div className={"grid items-end"}>
                 <div className={"flex flex-col"}>
                     <div className={"text-5xl max-[850px]:text-4xl self-center"}> Langara <span className={"text-lang-orange"}>Hacks</span></div>
+                    
+                    <div className={"text-5s max-[850px]:text-4s self-center"}> Collaborate, Create, Innovate </div>
                 </div>
             </div>
             
             <div className={"grid grid-cols-2"}>
                 <div className={"justify-self-end self-center w-[60vh] max-[850px]:w-[30vh]"}>
-                    <IconLabel iconSrc={"/location_icon.png"} label={"T Gallery, Langara"} height={25} width={25}/>
+                    <IconLabel iconSrc={"/location_icon.png"} label={"Langara College"} height={25} width={25}/>
                 </div> 
                 <div className={"justify-self-start self-center w-[40vh] max-[850px]:w-[20vh]"}>
                     <IconLabel iconSrc={"/calender_icon.png"} label={"Sept 23rd, 2023"} height={25} width={25}/>
