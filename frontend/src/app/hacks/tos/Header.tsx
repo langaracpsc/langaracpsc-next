2de48954aca792c4c049b3c052fb401a83c6c974
Anderson T <tseng.andersonn@gmail.com>
2023-09-17 14:07:49 -0700

    make header link back to main page

--- frontend/src/app/hacks/tos/Header.tsx
+++ frontend/src/app/hacks/tos/Header.tsx
@@ -7,7 +7,7 @@ export function Header()
         <div className = {"grid grid-rows-1 gap-1"}>
             <div className={"grid items-end mt-2"}>
                 <div className={"flex flex-col gap-2"}>
-                    <div className={"text-6xl max-[1100px]:text-3xl  self-center"}> Langara <span className={"text-lang-orange"}>Hacks</span></div>
+                    <div className={"text-6xl max-[1100px]:text-3xl  self-center"}> <a href="/hacks">Langara <span className={"text-lang-orange"}>Hacks</span></a></div>
                     <div className={"text-s max-[850px]:text-sm self-center"}> Collaborate, Create, Innovate </div>
                 </div>
             </div>
