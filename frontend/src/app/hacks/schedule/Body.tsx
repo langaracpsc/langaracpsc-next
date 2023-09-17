14e64c2ad574cb60b7482089d7c8ba1a01c8399d
rishit-singh <rsrishitsingh@gmail.com>
2023-09-17 13:01:18 -0700

    Added /hacks/schedule

---
+++ frontend/src/app/hacks/schedule/Body.tsx
@@ -0,0 +1,14 @@
+import {Timer} from "@/app/hacks/Timer";
+import {SocialIcons} from "@/app/SocialIcons";
+
+export function Body()
+{
+    return <div className={"grid-rows-2 bg-hacks-bg-black items-center"}>
+        <div className={"flex flex-col items-center mt-3"}>
+            <span className={"text-3xl max-[850px]:text-xl"}>Schedule</span>
+            <div className={"flex flex-col items-center justify-center gap-[7vh] max-[850px]:gap-[8vh] mt-[4vh] max-[850px]:mt-[9vh] max-h-full h-[100vh]"}>
+                <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRyF8-IjcBAjemcPYusfUjDub2ZY1qsMOeYzVBfq3bZrHJn0RQ498RVPvAz45TQUmaoGpWZMWGchkfV/pubhtml?gid=1141432285&amp;single=true&amp;widget=true&amp;headers=false" className={"h-full w-[50vw]"}></iframe>
+            </div>
+        </div>
+    </div>;
+}
---
+++ frontend/src/app/hacks/schedule/page.tsx
@@ -0,0 +1,10 @@
+import {Body} from "./Body";
+import {Header} from "./Header";
+ 
+export default function Page()
+{
+    return <div className={"grid grid-rows bg-navs-bg-black h-full font-audiowide"}>
+        <Header/>
+        <Body/>
+    </div>;
+} 
