f22b3cd80013ab19b74ac297a5f0ca92a2a1d7ae
JakeBareng <barengjake@gmail.com>
2023-09-20 12:01:05 -0700

    removed right margin on mobile view

--- frontend/src/app/NavBar.tsx
+++ frontend/src/app/NavBar.tsx
@@ -61,13 +61,14 @@ export class NavBar extends Component<NavBarProps, NavBarState>
         <div className={"mt-4"}></div>;
         <div className={"mt-3"}></div>;
         
-        return <div className="flex flex-row max-[600px]:grid max-[600px]:grid-rows-2  row-start-1 row-span-1 max-[600px]:justify-center">
+        return (
+            <div className="flex flex-row max-[600px]:grid max-[600px]:grid-rows-2  row-start-1 row-span-1 max-[600px]:justify-center">
                 <div className="min-[600px]:basis-1/4 max-[600px]:row-start-1 flex flex-row shrink-0 grow-0 max-[600px]:justify-center ">
                     <div></div>
                     <img src="LCS.png" alt="club-logo" className="ml-5 flex-start-2 max-[600px]:ml-8 shrink-0 grow-0 h-full select-none"/>
                     <div></div>
                 </div>
-                <div className="min-[600px]:basis-3/4 max-[600px]:grid max-[600px]:grid-template-cols[5%_90%_5%] flex justify-end mr-8 h-full">
+                <div className="min-[600px]:basis-3/4 max-[600px]:grid max-[600px]:grid-template-cols[5%_90%_5%] min-[600px]:flex min-[600px]:justify-end min-[600px]:mr-8 h-full">
                     <div></div>
                     <div className={"col-start-2"}>
                         <div className={"nav_button_container flex max-[500px]:gap-x-3 gap-x-7 mt-5 max-[500px]:mt-6"}>
@@ -78,8 +79,9 @@ export class NavBar extends Component<NavBarProps, NavBarState>
                         </div>
                     </div>
                     <div></div>
+                </div>
             </div>
-        </div>;
+        );
     } 
 }
 
