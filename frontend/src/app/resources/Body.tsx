8bcd09d055ca2dc668fccedaa9b369cf2cb36eb7
rishit-singh <rsrishitsingh@gmail.com>
2023-11-24 17:47:14 -0800

    Fixed /resources layout

--- frontend/src/app/resources/Body.tsx
+++ frontend/src/app/resources/Body.tsx
@@ -7,7 +7,7 @@ export function Body()
         
         <div className={"flex flex-col items-center mt-3"}>
             <span className={"text-3xl max-[850px]:text-xl"}>Resources</span>
-            <div className={"flex flex-col items-center justify-center gap-[7vh] max-[850px]:gap-[8vh] mt-[4vh] max-[850px]:mt-[9vh] max-h-full h-[250vh]"}>
+            <div className={"flex flex-col items-center justify-center gap-[7vh] max-[850px]:gap-[8vh] mt-[4vh] max-[850px]:mt-[9vh] max-h-full h-[100vh]"}>
                 <iframe src="https://docs.google.com/document/d/e/2PACX-1vQhTudGVmLYBP0q5ir1T9YYtfAN1MaKVEbbKOfhnGloD_DK5ff15Kwqe_jbL0sBrxivomKbfwG9EPsA/pub?embedded=true" className={"h-full w-[max(52vw,_500px)]"}></iframe>
             </div>
         </div>
