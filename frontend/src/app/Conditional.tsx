560b030798dec390428e319bd89d327ed4d4d0be
rishit-singh <rsrishitsingh@gmail.com>
2023-12-28 13:38:11 -0800

    Fixed Conditional

--- frontend/src/app/Conditional.tsx
+++ frontend/src/app/Conditional.tsx
@@ -10,7 +10,7 @@ interface ConditionalProps
 export default function Conditional({Condition, children} : ConditionalProps): JSX.Element 
 {
     if (Condition)
-        return (<>children</>);
+        return (<>{children}</>);
     
     return <></>;
 } 
