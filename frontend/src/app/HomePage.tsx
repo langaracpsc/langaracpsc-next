e18192c30158bfb40d81e07780c9ee3a0872f427
rishit-singh <rsrishitsingh@gmail.com>
2023-12-28 02:32:24 -0800

    Fixed HomePage image scaling

--- frontend/src/app/HomePage.tsx
+++ frontend/src/app/HomePage.tsx
@@ -53,10 +53,10 @@ export default function HomePage({} : HomePageProps)
     <div className={"flex-col-reverse"}></div>;
         
     return (<div className={"flex flex-row max-[600px]:flex-col-reverse bg-body-gray w-full h-full"}>
-        <div className={"grid grid-rows-[15%_60%_20%] max-[600px]:grid-rows-[5%_60%_20%] w-full h-full gap-3"}>
+        <div className={"grid grid-rows-[15%_60%_20%] max-[600px]:grid-rows-[5%_60%_20%] w-full h-full gap-3 max-[900px]:basis-1/3"}>
             <div></div>
 
-            <div className={"flex flex-row row-start-2 flex-col row-span-1 text-7xl max-[850px]:text-4xl max-[600px]:text-3xl text-white-900 justify-center"}>
+            <div className={"flex flex-row row-start-2 flex-col row-span-1 text-7xl max-[900px]:text-5xl max-[600px]:text-3xl text-white-900 justify-center"}>
                 <div className={flexContainer}>   
                     <span className="shrink text-lang-orange">Langara</span> 
                 </div>
@@ -75,12 +75,9 @@ export default function HomePage({} : HomePageProps)
             </div>
         </div>
         <div className="grid grid-rows-[20%_60%_20%] max-[600px]:grid-rows-[15%_60%_10%] max-[600px]:grid-rows-1 w-full h-full">
-            {/* {(this.state.Dimensions.X < 600) ? null : <div className={"text-4xl max-[600px]:invisible"}></div>} */}
-            
             <div className={"flex flex-row row-start-2 row-span-1 text-white-900 justify-center max-[600px]:mt-0"}>
-                <img className={"max-[600px]:h-[130%] max-[600px]:w-[100%] shrink-0"} src={"code_snippet.png"}/>
+                <img className={"max-[600px]:h-[160%] max-[600px]:w-[100%] shrink-0"} src={"code_snippet.png"}/>
             </div>
-            {/* {(this.state.Dimensions.X < 600) ? null : <div className={"text-7xl max-[600px]:invisible"}></div>} */}
         </div>
     </div>
     );
