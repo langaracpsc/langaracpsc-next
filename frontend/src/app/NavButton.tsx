5f6aaaad57b61faaaa9a508d4bd4a2de87efb08f
rishit-singh <rsrishitsingh@gmail.com>
2023-12-27 22:44:22 -0800

    Added a cache element for lang-orange

--- frontend/src/app/NavButton.tsx
+++ frontend/src/app/NavButton.tsx
@@ -20,11 +20,14 @@ interface NavButtonProps
 export default function NavButton({ Label, EndPoint, Activated, OnClick } : NavButtonProps): JSX.Element 
 {
     const color: string = (Activated) ? "white" : "lang-orange";
+    <div className="border-lang-orange"></div> // cache
 
-    return (<><button 
-            className={`bg-transparent max-[600px]:text-[10px] max-[600px]:h-8 max-[600px]:w-15 h-10 text-white font-semibold hover:text-white py-2 px-4 border border-${color} hover:border-white rounded select-none`}
-            onClick={() => {OnClick(EndPoint);}}>
-            {Label}
-        </button></>);
+    return (<>
+            <button 
+                className={`bg-transparent max-[600px]:text-[10px] max-[600px]:h-8 max-[600px]:w-15 h-10 text-white font-semibold hover:text-white py-2 px-4 border border-${color} hover:border-white rounded select-none`}
+                onClick={() => {OnClick(EndPoint);}}>
+                {Label}
+            </button>
+        </>);
 }
     
