6d3b58f1774d43ff328e0cef679fd257486398c0
rishit-singh <rsrishitsingh@gmail.com>
2024-02-29 11:22:51 -0800

    Changed the basic footer layout

--- frontend/src/app/Footer.tsx
+++ frontend/src/app/Footer.tsx
@@ -58,20 +58,23 @@ export default function Footer()
     }, [resourcesMap, linksMap]);
 
     return (<>
-        <div className="grid grid-cols-3 bg-[#1E1E1E] p-4 justify-center">
-            <div className="flex flex-col">
-                <div className="ml-[12vh] max-[800px]:ml-0">
-                    <div className={"font-bold"}>Resources</div>
-                    <div className={"flex flex-col text-sm gap-3 mt-3"}>
-                        {resources}
+        <div className="grid grid-cols-2 bg-[#1E1E1E] p-6 justify-center">
+            <div className="flex flex-row">
+
+                <div className="flex flex-col">
+                    <div className="ml-[8vh] max-[800px]:ml-0">
+                        <div className={"font-bold"}>Resources</div>
+                        <div className={"flex flex-col text-sm gap-3 mt-3"}>
+                            {resources}
+                        </div>
                     </div>
+                    <div className="mt-6 ml-5 text-xs">Copyright © {new Date(Date.now()).getFullYear()} Langara Computer Science Club</div>
                 </div>
-                <div className="mt-6 ml-5 text-xs">Copyright © {new Date(Date.now()).getFullYear()} Langara Computer Science Club</div>
-            </div>
-            <div className="flex flex-col max-[800px]:items-center">
-                <div className={"font-bold"}>Links</div>
-                <div className={"flex flex-col gap-1 mt-1 "}>
-                    {links}
+                <div className="flex flex-col max-[800px]:items-center">
+                    <div className={"font-bold"}>Links</div>
+                    <div className={"flex flex-col gap-1 mt-1 "}>
+                        {links}
+                    </div>
                 </div>
             </div>
             <div className="flex flex-col items-end max-[600px]:items-center max-[600px]:mr-0 mr-10">
@@ -87,9 +90,8 @@ export default function Footer()
                         <div>
                         Connect
                         </div> 
-                        <div className="gap-3">
-                            <SocialIcons height={25} width={25}/>
-                        </div>
+                        
+                        <SocialIcons height={25} width={25} gap={2}/>
                     </div>
                     <div className={"flex flex-row max-[600px]:flex-col gap-3 hover:text-lang-orange"}>
                         <div>
--- frontend/src/app/SocialIcons.tsx
+++ frontend/src/app/SocialIcons.tsx
@@ -5,30 +5,34 @@ import { Component } from "react";
 
 interface SocialIconsProps {
     height?: number,
-    width?: number 
+    width?: number,
+    gap?: number
 }
 
-export default function SocialIcons({height, width}: SocialIconsProps): JSX.Element {
+export default function SocialIcons({height, width, gap}: SocialIconsProps): JSX.Element {
     const Socials: Array<string> = [
         "https://github.com/langaracpsc/",
         "https://discord.gg/langara-computer-science-club-753037165050593300",
         "https://www.instagram.com/langaracpsc/"
     ];
 
+    if (gap === undefined)
+        gap = 0;
+
     return (
-        <>
+        <div className={`flex flex-row gap-${gap}`}>
             {Socials.map((name, index) => (
                 <SocialIcon 
                     key={index} 
                     url={name} 
                     fgColor={"#FFFFFF"} 
                     bgColor={"#F15A22"} 
-                    style={{ height: (height === undefined) ? 50 : height, width: (width === undefined) ? 50 : width}}
+                    style={{ height: (height === undefined) ? 50 : height, width: (width === undefined) ? 50 : width }}
                     target="_blank" 
                     rel="noopener noreferrer"
                     className="transition duration-1000 transform hover:scale-110"
                 />
             ))}
-        </>
+        </div>
     ); 
 }
