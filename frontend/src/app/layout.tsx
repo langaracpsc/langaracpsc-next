b8a2c8cd14459465ca8349c67df1ff07413d21cb
Anderson T <tseng.andersonn@gmail.com>
2024-09-13 04:52:30 -0700

    add better plausible integration

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 
 import '/styles/globals.css'
+import PlausibleProvider from 'next-plausible'
 
 export const metadata = {
   title: "Langara Computer Science Club",
@@ -13,6 +14,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+        <head>
+          <PlausibleProvider domain="langaracs.ca" customDomain='https://plausible.langaracs.ca' trackOutboundLinks selfHosted/>
+        </head>
         <body> 
           {children}
         </body>
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -38,11 +38,6 @@ export default function Home() {
             <div className='h-screen w-full bg-body-gray'>
 
 
-                <script defer data-domain="langaracs.ca" src="https://plausible.langaracs.ca/js/script.js"></script>
-
-
-
-
                 <main className="grid grid-rows-[10vh_1fr] dark max-[600px]:grid-rows-[20vh_1fr] h-full w-full overflow-y-visible overflow-x-hidden">
                     <NavBar Pages={new Map<string, string>([
                         ["Home", "/"],
@@ -50,29 +45,14 @@ export default function Home() {
                         ["Events", "/events"]
                     ])} />
 
-
-
                     <HomePage />
 
                     <Footer />
                 </main>
-
-
-
-
-
-
-
-
-
-                
-
-
-
             </div>
-
+            
+        {/* Redux Stuff */}
         </Provider>
-
     );
 }
 
