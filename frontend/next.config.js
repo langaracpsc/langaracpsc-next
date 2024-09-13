a328e07cf106153d538c0afcbdff9490ddc95855
Anderson T <tseng.andersonn@gmail.com>
2024-09-13 02:10:04 -0700

    remove more references to the former webapi

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,12 +4,6 @@ const nextConfig = {
     appDir: true,
   },
   env: {
-    APIURL: process.env.APIURL,
-    APIKEY: process.env.APIKEY,
-    CALENDAR_API_KEY: process.env.CALENDAR_API_KEY,
-    CALENDAR_ID: process.env.CALENDAR_ID,
-    CALENDAR_BASE_URL: process.env.CALENDAR_BASE_URL,
-    CALENDAR_OAUTH_KEY: process.env.CALENDAR_OAUTH_KEY
   }, 
   images: {
     remotePatterns: [
