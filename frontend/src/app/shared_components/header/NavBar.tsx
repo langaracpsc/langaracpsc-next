d3e5409d2faade14d499bfe3a9622ce88078e864
Anderson T <tseng.andersonn@gmail.com>
2024-12-03 09:52:26 -0800

    minor navbar changes

--- frontend/src/app/shared_components/header/NavBar.tsx
+++ frontend/src/app/shared_components/header/NavBar.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import Menu from "../Menu";
 import NavButton from "./NavButton";
+import Image from "next/image";
 
 export default function NavBar({ hasHeight = true }) {
     const navButtons = (
@@ -20,17 +21,23 @@ export default function NavBar({ hasHeight = true }) {
                 <div className="flex flex-row justify-between items-center mx-auto px-4 md:px-10">
                     <div className="flex flex-row items-center justify-start">
                         <Link href="/">
-                            <img src="LCS.png" alt="club-logo" className="shrink-0 grow-0 w-16 h-20 cursor-pointer select-none object-cover" />
+                            <Image
+                                src="/LCS.png"
+                                alt="LCSC Logo"
+                                width={100}
+                                height={100}
+                                className="shrink-0 grow-0 w-16 h-20 cursor-pointer select-none object-cover"
+                            />
                         </Link>
-                        <div className="justify-center items-start lg:justify-start lg:flex hidden">
+                        <div className="justify-center items-start lg:justify-start md:flex gap-2 hidden">
                             {navButtons}
                         </div>
                     </div>
-                    <div className="lg:flex hidden justify-end items-center h-full">
+                    <div className="md:flex hidden justify-end items-center h-full">
                         <div className="flex items-center gap-x-7">
                         </div>
                     </div>
-                    <div className="dropdown lg:hidden">
+                    <div className="dropdown md:hidden">
                         <Menu Buttons={Array.isArray(navButtons) ? navButtons : [navButtons]} />
                     </div>
                 </div>
--- frontend/src/app/shared_components/header/NavButton.tsx
+++ frontend/src/app/shared_components/header/NavButton.tsx
@@ -10,7 +10,7 @@ interface NavButtonProps {
 
 export default function NavButton({ Label, EndPoint }: NavButtonProps) {
     return (
-        <Link href={EndPoint} className={`text-xl font-bold mx-2 px-4 py-2 transition duration-300
+        <Link href={EndPoint} className={`text-xl font-bold p-4 transition duration-300
             "text-lang-orange" hover:scale-110`}>
                 {Label}
         </Link>
